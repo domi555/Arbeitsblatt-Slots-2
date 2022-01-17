d87f2bbabf64fdef30c447bb1288015275bef7f8
domi555 <66876360+domi555@users.noreply.github.com>
2022-01-17 15:40:01 +0100

    Update

---
+++ Slots 2 START/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container mt-5" id="app">
+    <Catogram :cats="cats">
+      <template #boss="{item}">
+        <h5>{{ item.name }}</h5>
+        <img :src="item.image" width="200" alt="" />
+      </template>
+      <template #employees="{item}">
+        <div>
+          <h5>{{ item.name }}</h5>
+          <img :src="item.image" width="200" alt="" />
+          <p class="mt-2 fw-bold">Job role: {{ item.role }}</p>
+        </div>
+      </template>
+    </Catogram>
+  </div>
+</template>
+
+<script>
+import cats from '@/assets/cats.json';
+
+import Catogram from '@/components/Catogram.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Catogram,
+  },
+  data() {
+    return {
+      cats,
+    };
+  },
+};
+</script>
---
+++ Slots 2 START/src/components/Catogram.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>Scratchy Inc.</h3>
+
+    <h4>Boss</h4>
+    <slot name="boss" :item="boss"> </slot>
+
+    <h4 class="mt-4">Employees</h4>
+    <div class="d-flex flex-row">
+      <div v-for="(cat, i) of employees" :key="i" class="me-3">
+        <slot name="employees" :item="cat"> </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cats: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      boss: this.cats.find((el) => el.role == 'Boss'),
+      employees: this.cats.filter((el) => el.role != 'Boss'),
+    };
+  },
+};
+</script>
+
+<style scoped></style>
